aa6e0a6d2576aa4fb83ae0149bb756d8570ee889
Shurui-Liu <shurui.liu.710@gmail.com>
2026-03-16 08:23:20 -0700

    Update main.go to change ProductID type from string to int and add Name field to Item struct. Update binary bootstrap file.

--- week7/lambda/main.go
+++ week7/lambda/main.go
@@ -11,7 +11,8 @@ import (
 )
 
 type Item struct {
-	ProductID string  `json:"product_id"`
+	ProductID int     `json:"product_id"`
+	Name      string  `json:"name"`
 	Quantity  int     `json:"quantity"`
 	Price     float64 `json:"price"`
 }
